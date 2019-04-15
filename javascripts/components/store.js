70ca17302a86aa93280c291c449c6120540e7d5c
David Mullins <Mullins6323@gmail.com>
2019-04-15 01:57:14 -0500

    updated

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,22 +1,24 @@
 import cart from './cart.js';
 
-import meats from '../components/meat.js';
+import meat from '../components/meat.js';
 import util from '../helpers/util.js';
 
-const addToCartEvent = (e) => {
-    e.preventDefault();
-    const meatPrice = meats.getPrice();
-    cart.setCart(meatPrice);
-    cart.cartToDom();
-};
+// const addToCartEvent = (e) => {
+//     e.preventDefault();
+//     const meatPrice = meats.getPrice();
+//     cart.setCart(meatPrice);
+//     cart.cartToDom();
+// };
 
 const makeStore = () => {
-    const meatPrice = meat.getPrice();
-    let domString = '<h2>Our only book</h2>';
-    domString += `<h3>${meatPrice.value}`
-    domString += `<button id="cart-button" class="btn btn-danger">Add to Cart</button>`
-    util.printToDom('store-container', domString);
-    document.getElementById('cart-button').addEventListener("click", addToCartEvent);
+    // const meatPrice = meat.getPrice();
+    // let domString = '<h2>Our only book</h2>';
+    // domString += `<h3>${meatPrice.value}`
+    // domString += `<button id="cart-button" class="btn btn-danger">Add to Cart</button>`
+    // util.printToDom('store-container', domString);
+    // document.getElementById('cart-button').addEventListener("click", addToCartEvent);
+    console.log(meat.getType());
+    console.log(meat.getPrice()['tuna']);
 };
 
 export default { makeStore };
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import buttonEvents from './javascripts/helpers/buttonEvents.js';
+import makeStore from './javascripts/components/store.js';
 
 const init = () => {
     buttonEvents.attachEvents();
+    makeStore.makeStore();
 };
 
 init();
